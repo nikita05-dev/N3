a4b2d5ad6ec85e8f6bbf5418a7f82ebe96845409
nikita05-dev <nikita0404hornstein@gmail.com>
2026-08-18 10:43:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nbg3-report-v6";
+const CACHE = "nbg3-report-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
